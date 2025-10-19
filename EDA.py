d3dbd452c4d05b5fbcfc95ba99a53a1844df9550
Anmol7860-bit <anmolbhandare7860@gmail.com>
2025-10-19 09:49:24 +0530

    basic exploration

--- EDA.py
+++ EDA.py
@@ -7,3 +7,20 @@ warnings.filterwarnings("ignore")
 penguin=pd.read_csv('penguins.csv')
 print(penguin.head())
 print(penguin.shape)
+#delete the nan values/columns which are not required 
+print(penguin.dropna(inplace=True)) 
+#coloumns which are not important to your case study drop it 
+a=penguin.drop(['year','sex'],axis=1)
+print(a)
+#nunique function to count unique values in each column 
+print(a.nunique())
+# drop duplicates()
+print(a.drop_duplicates())
+print(a.describe(include='all'))
+#info function to get the information about the data types and non null values
+print(a.info())
+print(a.rename(columns={'species':'Species','island':'Island'}))
+BMI_unique=a.body_mass_g.unique()
+print(BMI_unique)
+
+#dealing with missing values
