4d4092042eef8eedc24a109c974bfe3eea7e4e39
Anmol7860-bit <anmolbhandare7860@gmail.com>
2025-10-26 23:48:26 +0530

    changed the data to a more reliable one

--- EDA.py
+++ EDA.py
@@ -4,31 +4,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 import warnings
 warnings.filterwarnings("ignore")
-penguin=pd.read_csv('penguins_iter.csv')
+penguin=pd.read_csv('penguins_lter.csv')
 print(penguin.head())
 print(penguin.shape)
 #delete the nan values/columns which are not required 
-print(penguin.dropna(inplace=True)) 
+print(penguin.dropna(inplace=True))
 #coloumns which are not important to your case study drop it 
-a=penguin.drop(['year','sex'],axis=1)
+a=penguin.drop(['studyName','Sample Number','Stage','Region','Individual ID','Date Egg','Delta 15 N (o/oo)','Delta 13 C (o/oo)','Comments'],axis=1)
 print(a)
 #nunique function to count unique values in each column 
-print(a.nunique())
+#print(a.nunique())
 # drop duplicates()
-print(a.drop_duplicates())
-print(a.describe(include='all'))
+#print(a.drop_duplicates())
+#print(a.describe(include='all'))
 #info function to get the information about the data types and non null values
 #print(a.info())
-print(a.rename(columns={'species':'Species','island':'Island'}))
+#print(a.rename(columns={'species':'Species','island':'Island'}))
 #BMI_unique=a.body_mass_g.unique()
 #print(BMI_unique)
 
 #dealing with missing values
 #replacing with NaN if there is any '\.+-" such values 
-print(a.replace('\.+-' ,np.nan,regex=True))
+#print(a.replace('\.+-' ,np.nan,regex=True))
 
-print(a.isnull().sum())
-print(a.head())
+#print(a.isnull().sum())
+#print(a.head())
 #reviewing each coloumn datatype
-print(a.info())
+#print(a.info())
 
