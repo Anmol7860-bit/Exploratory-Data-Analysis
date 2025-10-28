9de0748feb203d5c60d9ffee91bcfc22399b5a6c
Anmol7860-bit <anmolbhandare7860@gmail.com>
2025-10-28 23:49:44 +0530

    Dealing with missing values
    removed the old data to the new one and and started to deal with missing values.

--- EDA.py
+++ EDA.py
@@ -7,28 +7,36 @@ warnings.filterwarnings("ignore")
 penguin=pd.read_csv('penguins_lter.csv')
 print(penguin.head())
 print(penguin.shape)
-#delete the nan values/columns which are not required 
+#delete the nan rows/columns which are not required which is done inplace
 print(penguin.dropna(inplace=True))
 #coloumns which are not important to your case study drop it 
 a=penguin.drop(['studyName','Sample Number','Stage','Region','Individual ID','Date Egg','Delta 15 N (o/oo)','Delta 13 C (o/oo)','Comments'],axis=1)
 print(a)
 #nunique function to count unique values in each column 
-#print(a.nunique())
+print(a.nunique())
 # drop duplicates()
-#print(a.drop_duplicates())
-#print(a.describe(include='all'))
+print(a.drop_duplicates())
+print(a.describe(include='all'))
 #info function to get the information about the data types and non null values
-#print(a.info())
-#print(a.rename(columns={'species':'Species','island':'Island'}))
-#BMI_unique=a.body_mass_g.unique()
-#print(BMI_unique)
+print(a.info())
+#Rename the miss spelled words or titles
+a=a.rename(columns={'Clutch Completion':'Clutch_Completion','Body Mass (g)':'Body_Mass(g)'})
+print(a)
+#unique values in body mass coloumn to determine whether the sample size is of same size and age 
+print(a.info())
+BMI_Unique=a['Body_Mass(g)'].unique()
+print(BMI_Unique)
 
 #dealing with missing values
-#replacing with NaN if there is any '\.+-" such values 
-#print(a.replace('\.+-' ,np.nan,regex=True))
 
-#print(a.isnull().sum())
-#print(a.head())
+#replacing with NaN if there is any '\.+-" such values 
+print(a.replace('\.+-' ,np.nan,regex=True))
+
+print(a.isnull().sum())
+print(a.head())
 #reviewing each coloumn datatype
-#print(a.info())
+print(a.info())
+
+#some data contains categorical values in the form of 0 or 1 to convert this into numeric we use pd.to_numeric function
+a['Body_Mass(g)']=pd.to_numeric(a['Body_Mass(g)'],errors='coerce')
 
