fb48cc4a254600c985c795d155d5f680c63f6f6c
Anmol7860-bit <anmolbhandare7860@gmail.com>
2025-11-19 22:22:17 +0530

    visualize the data for distribution

--- Outlier.py
+++ Outlier.py
@@ -10,3 +10,9 @@ from scipy import stats
 warnings.filterwarnings("ignore")
 diabetes=pd.read_csv('diabetes.csv')
 print(diabetes.head())
+
+#first and foremost visualize the data to understand the distribution of data
+sns.histplot(data=diabetes['Age'],kde=True)
+plt.axvline(x=diabetes['Age'].mean(),color='red',alpha=0.5,label='Mean')
+plt.axvline(x=diabetes['Age'].median(),c='blue',ls='--',alpha=0.5,label='Median')
+plt.legend()
